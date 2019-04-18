61d2a976efd8c015f80559ea266fad3b384db87a
Dr_Mike_Cooper <drmikec@aie.edu.au>
2019-04-18 15:38:48 +0930

    Initial Commit

---
+++ Assets/Aligner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aligner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // use the oculus go forward direction
+        //transform.position = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+        transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+    }
+}
---
+++ Assets/Clicker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+public class Clicker : MonoBehaviour {
+
+    public NavMeshAgent nv;
+    public Transform controller;
+    public Transform marker;
+   
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update () {
+
+        //if (EventSystem.current.IsPointerOverGameObject() == false)
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(controller.position, controller.forward);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (marker)
+                    marker.position = hit.point;
+
+                if (Input.GetMouseButtonDown(0))
+                    nv.SetDestination(hit.point);
+            }
+        }
+	}
+}
